e7da006855f5f5c1e090299fc965286537840bba
Mikkel Vestergaard <juulvestergaard@hotmail.com>
2025-09-16 17:23:35 +0200

    Updated vim setup

--- configuration.nix
+++ configuration.nix
@@ -91,7 +91,6 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neovim
     wget
     kitty
