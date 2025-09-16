18466936e819eaabee6bce2d3a30e1423383b50b
Mikkel Vestergaard <juulvestergaard@hotmail.com>
2025-09-16 16:51:04 +0200

    Initial NixOS commit

---
+++ home.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  # Setup user and home
+  home.username = "moggel";
+  home.homeDirectory = "/home/moggel";
+
+  # Setup config links
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+  home.file.".gitconfig".source = ./gitconfig;
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo I use NixOS, btw";
+    };
+  };
+
+  # Setup user packages
+  home.packages = with pkgs; [
+    btop
+    fzf
+    nmap
+  ];
+
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards 
+  # incompatible changes.
+  # 
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version 
+  # changes in each release.
+  home.stateVersion = "25.05";
+}
